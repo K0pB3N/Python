79992231649fa168f4a174649ec171337d22d6c3
Ri7vaI <Altair1-3@yandex.ru>
2022-11-09 02:16:54 +0300

    Pr9 changed

--- Pr9.py
+++ Pr9.py
@@ -19,18 +19,22 @@ target = data.target
 target_names = data.target_names
 
 def f1():
-    print(predictors.head(5), '\n\nЦелевая переменная:\n', target.head(5), '\n\nИмена классов:\n', target_names)
+    # 1. Найти данные для классификации. Данные в группе повторяться не должны! Предобработать данные, если это необходимо.
+    print(predictors.head(5), '\n\nЦелевая переменная\n', target.head(5), '\nИмена классов:\n', target_names)
 
 f1()
 
 def f2():
+    # 2. Изобразить гистограмму, которая показывает баланс классов. Сделать выводы
     target.hist(bins=20,  figsize=(7, 7))
     plt.xticks([0, 1])
     plt.show()
 f2()
 
+print('\n')
 
 def f3():
+    # 3. Разбить выборку на тренировочную и тестовую. Тренировочная для обучения модели, тестовая для проверки ее качества.
     x_train, x_test, y_train, y_test = train_test_split(predictors,
                                                         target,
                                                         train_size=0.8,
@@ -39,16 +43,16 @@ def f3():
     print(' Размер для признаков обучающей выборки', x_train.shape, '\n',
         'Размер для признаков тестовой выборки', x_test.shape, '\n',
         'Размер для целевого показателя обучающей выборки', y_train.shape, '\n',
-        'Размер для показателя тестовой выборки', y_test.shape)
+        'Размер для показателя тестовой выборки', y_test.shape, '\n')
 
-
+    # 4. Применить алгоритмы классификации: логистическая регрессия
     start_time = time.time()
     model = LogisticRegression(random_state=271)
-    model.fit(x_train, y_train)
+    print(model.fit(x_train, y_train))
     print('\n')
 
     y_predict = model.predict(x_test)
-    print("--- %s seconds ---" % (time.time() - start_time))
+    print("--- %s seconds ---" % (time.time() - start_time), '\n')
     print(y_predict)
     print(np.array(y_test))
     print('\n')
@@ -60,41 +64,43 @@ def f3():
     print(classification_report(y_test, y_predict))
 
     print('\n')
+    
+    # 4. Применить алгоритмы классификации: SVM
     start_time = time.time()
     param_kernel = ('linear', 'rbf', 'poly', 'sigmoid')
     parameters = {'kernel': param_kernel}
     model = SVC()
     grid_search_svm = GridSearchCV(estimator=model, param_grid=parameters, cv=6)
-    grid_search_svm.fit(x_train, y_train)
+    print(grid_search_svm.fit(x_train, y_train), '\n')
     best_model = grid_search_svm.best_estimator_
-    print(best_model.kernel)
+    print('Best model: ', best_model.kernel)
     print('\n')
     svm_preds = best_model.predict(x_test)
-    print("--- %s seconds ---" % (time.time() - start_time))
+    print("--- %s seconds ---" % (time.time() - start_time), '\n')
     print(svm_preds)
     print('\n')
-    print(classification_report(svm_preds,y_test))
+    print(classification_report(svm_preds, y_test))
 
     fig = px.imshow(confusion_matrix(y_test, svm_preds), text_auto=True)
     fig.update_layout(xaxis_title='Target (SVM)', yaxis_title='Prediction')
     fig.show()
 
-
+    # 4. Применить алгоритмы классификации: KNN
     start_time = time.time()
     number_of_neighbors = np.arange(3, 10, 25)
     model_KNN = KNeighborsClassifier()
     params = {'n_neighbors': number_of_neighbors}
-
-    grid_search = GridSearchCV(estimator=model_KNN, param_grid=params, cv=6)
-    grid_search.fit(x_train, y_train)
-    print(grid_search.best_score_)
     print('\n')
-    print(grid_search.best_estimator_)
+    grid_search = GridSearchCV(estimator=model_KNN, param_grid=params, cv=6)
+    print(grid_search.fit(x_train, y_train), '\n')
+    print('Best score: ',grid_search.best_score_)
+    print('\n')
+    print('Best estimator: ', grid_search.best_estimator_)
     print('\n')
     knn_preds = grid_search.predict(x_test)
-    print("--- %s seconds ---" % (time.time() - start_time))
+    print("--- %s seconds ---" % (time.time() - start_time), '\n')
     print('\n')
-    print(classification_report(knn_preds,y_test))
+    print(classification_report(knn_preds, y_test))
     
     print('\n')
     fig = px.imshow(confusion_matrix(y_test, knn_preds), text_auto=True)
